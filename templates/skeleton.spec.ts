710cd6aa08c522aa2439d1e89970122c83027e74
Jeremy Zagorski <jmzagorski@gmail.com>
2020-11-08 11:22:41 -0500

    feat(templates): Add skeleton files

---
+++ templates/skeleton.spec.ts
@@ -0,0 +1,20 @@
+import {bootstrap} from 'aurelia-bootstrapper';
+import {StageComponent} from 'aurelia-testing';
+import {PLATFORM} from 'aurelia-pal';
+
+describe('Stage App Component', () => {
+  let component;
+
+  beforeEach(() => {
+    component = StageComponent
+      .withResources(PLATFORM.moduleName('app'))
+      .inView('<app></app>');
+  });
+
+  afterEach(() => component.dispose());
+
+  it('should render message', async () => {
+    await component.create(bootstrap);
+    fail("Setup this test fool!")
+  });
+});
