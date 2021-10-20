ee02f2581fdd44e52dd11943010e9c3dab5005d0
Jeremy Zagorski <jmzagorski@protonmail.com>
2021-10-20 02:22:36 +0000

    refactor(script): Add args for visual studio version
    Add year and version, hope path stays somewhat the same

--- scripts/install.cmd
+++ scripts/install.cmd
@@ -1,1 +1,13 @@
-"%comspec%" /k ""%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat" && "%cd%\build.cmd" && vim "%cd%\dummy.cs" +PackUpdate +OmniSharpInstall +:q"
+@echo off
+
+IF [%1] == [] (
+    echo "Visual studio year must be provided (e.g. 2019)"
+    exit /b %errorlevel%
+)
+
+IF [%2] == [] (
+    echo "Visual studio edition must be provided (e.g. Community)"
+    exit /b %errorlevel%
+)
+
+"%comspec%" /k ""%programfiles(x86)%\Microsoft Visual Studio\%1\%2\Common7\Tools\VsDevCmd.bat" && "%cd%\build.cmd" && vim "%cd%\dummy.cs" +PackUpdate +OmniSharpInstall +:q"
